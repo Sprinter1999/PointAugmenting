60efb62c1453270f8cf16f4ccd8f1b39e2d870bc
Xuefen <215437588@qq.com>
2024-01-12 14:32:45 +0800

    Update __init__.py

--- det3d/datasets/pipelines/__init__.py
+++ det3d/datasets/pipelines/__init__.py
@@ -6,6 +6,8 @@ from .loading import *
 from .test_aug import DoubleFlip
 from .preprocess import Preprocess, Voxelization
 
+
+#TODO: load data and preprocess pipelines, 这里是所有需要用到的transforms
 __all__ = [
     "Compose",
     "to_tensor",
